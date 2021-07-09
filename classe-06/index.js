625c3023a04cbf9141738027f12c11c4a703ac5f
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-08 22:18:42 -0300

    Validação de numeros pares

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,2 +1,4 @@
-const numeros = [0, 122, 4, 6, 7, 8, 44];
+const numeros = [0, 122, 4, 5, 7, 8, 44];
 
+const resultado = numeros.every(x => (x % 2 === 0));
+console.log(resultado === false? "Array inválido" : "Array válido");
