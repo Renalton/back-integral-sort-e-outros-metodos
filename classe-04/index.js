33a48785b4f18e2cf8f901e3f2ebc27b894b9c4d
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-08 22:18:10 -0300

    Verificando limite de caracters

--- classe-04/index.js
+++ classe-04/index.js
@@ -1,1 +1,5 @@
 const palavras = ['livro', 'caneta', 'sol', 'carro', 'orelha'];
+
+const resultado = palavras.some((x) => x.length > 5);
+
+console.log(resultado === true? "existe palavra inválida" : "array validado");
