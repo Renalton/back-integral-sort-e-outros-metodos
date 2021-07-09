d8120d36e4f030759bd242f9387f09ad23344a1b
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-08 22:17:53 -0300

    Filtrar maior número

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,11 @@
-const numeros = [1, 98, -76, 0, 12, 19, 5, 60, 44];
+const numeros = [1, 98, -76, 0, 12, 19, 5, 60, 44, 100];
+//Filtrar maior número
+const resultado = numeros.reduce((acc, cur) => {
+
+    if( acc < cur){
+        acc = cur;
+    }
+    return acc;
+
+});
+console. log(resultado);
