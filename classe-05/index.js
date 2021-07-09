83f9428fb5f17eac8dac5f9eedb37d6c09d00c43
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-08 22:18:25 -0300

    Validando lista de compras

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,1 +1,7 @@
 const palavras = ["arroz", "feijão", "carne", "cerveja", "macarrão"];
+
+const resultado = palavras.some(x => x === "cerveja" || x === "vodika");
+
+const yes = "revise sua lista, joão. possui bebida com venda proibida!";
+const No = "tudo certo, vamos as compras!";
+console.log(resultado === true? yes : No);
